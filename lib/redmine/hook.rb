75c75ad51e8f0edb4bf231e850c25ae58805b976
Gonçalo Silva <goncalossilva@gmail.com>
2010-05-27 16:53:12 +0100

    Rails3.1: lib: replace "ActionController::UrlWriter" to "Rails.application.routes.url_helpers"
    --HG--
    extra : source : 3053a4d2820d02ca4efd1fc9352b4225edd31710

--- lib/redmine/hook.rb
+++ lib/redmine/hook.rb
@@ -14,7 +14,7 @@
 
 module Redmine
   module Hook
-    include ActionController::UrlWriter
+    include Rails.application.routes.url_helpers
 
     @@listener_classes = []
     @@listeners = nil
@@ -90,7 +90,7 @@ module Redmine
       include ActionView::Helpers::UrlHelper
       include ActionView::Helpers::AssetTagHelper
       include ActionView::Helpers::TextHelper
-      include ActionController::UrlWriter
+      include Rails.application.routes.url_helpers
       include ApplicationHelper
 
       # Default to creating links using only the path.  Subclasses can
