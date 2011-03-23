a987b74ed14e6ee67f84243e2ac24050c29c6f0a
Holger Just <h.just@finn.de>
2011-03-23 10:57:23 +0100

    [#230] Make loadpath of themes configurable

--- lib/redmine/themes.rb
+++ lib/redmine/themes.rb
@@ -5,33 +5,33 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module Redmine
   module Themes
-  
+
     # Return an array of installed themes
     def self.themes
       @@installed_themes ||= scan_themes
     end
-    
+
     # Rescan themes directory
     def self.rescan
       @@installed_themes = scan_themes
     end
-    
+
     # Return theme for given id, or nil if it's not found
     def self.theme(id, options={})
       return nil if id.blank?
-      
+
       found = themes.find {|t| t.id == id}
       if found.nil? && options[:rescan] != false
         rescan
@@ -39,11 +39,11 @@ module Redmine
       end
       found
     end
-  
+
     # Class used to represent a theme
     class Theme
       attr_reader :path, :name, :dir
-      
+
       def initialize(path)
         @path = path
         @dir = File.basename(path)
@@ -51,49 +51,60 @@ module Redmine
         @stylesheets = nil
         @javascripts = nil
       end
-      
+
       # Directory name used as the theme id
       def id; dir end
-      
+
       def ==(theme)
         theme.is_a?(Theme) && theme.dir == dir
       end
-      
+
       def <=>(theme)
         name <=> theme.name
       end
-      
+
       def stylesheets
         @stylesheets ||= assets("stylesheets", "css")
       end
-      
+
       def javascripts
         @javascripts ||= assets("javascripts", "js")
       end
-      
+
       def stylesheet_path(source)
         "/themes/#{dir}/stylesheets/#{source}"
       end
-      
+
       def javascript_path(source)
         "/themes/#{dir}/javascripts/#{source}"
       end
-      
+
       private
-      
+
       def assets(dir, ext)
         Dir.glob("#{path}/#{dir}/*.#{ext}").collect {|f| File.basename(f).gsub(/\.#{ext}$/, '')}
       end
     end
-    
+
     private
-    
+
     def self.scan_themes
-      dirs = Dir.glob("#{Rails.public_path}/themes/*").select do |f|
-        # A theme should at least override application.css
-        File.directory?(f) && File.exist?("#{f}/stylesheets/application.css")
-      end
-      dirs.collect {|dir| Theme.new(dir)}.sort
+      theme_paths.inject([]) do |themes, path|
+        dirs = Dir.glob(File.join(path, '*')).select do |f|
+          # A theme should at least override application.css
+          File.directory?(f) && File.exist?("#{f}/stylesheets/application.css")
+        end
+        themes += dirs.collect { |dir| Theme.new(dir) }
+      end.sort
+    end
+
+    def self.theme_paths
+      paths = Redmine::Configuration['themes_storage_path']
+      paths = [paths] unless paths.is_a?(Array)
+      paths.flatten!; paths.compact!
+
+      paths = ["#{Rails.public_path}/themes"] if paths.empty?
+      paths.collect { |p| File.expand_path(p, Rails.root) }
     end
   end
 end
@@ -105,7 +116,7 @@ module ApplicationHelper
     end
     @current_theme
   end
-  
+
   def stylesheet_path(source)
     if current_theme && current_theme.stylesheets.include?(source)
       super current_theme.stylesheet_path(source)
@@ -113,11 +124,11 @@ module ApplicationHelper
       super
     end
   end
-  
+
   def path_to_stylesheet(source)
     stylesheet_path source
   end
-  
+
   # Returns the header tags for the current theme
   def heads_for_theme
     if current_theme && current_theme.javascripts.include?('theme')
