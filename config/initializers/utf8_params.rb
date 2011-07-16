e5b6b512c87ad1de6b460a31d03de142c9ff8771
Gary Verhaegen <gary.verhaegen@gmail.com>
2011-07-16 10:08:03 +0200

    Fixes #524 by forcing the conversion of params to UTF-8

---
+++ config/initializers/utf8_params.rb
@@ -0,0 +1,20 @@
+raise "Check if this is still needed on " + Rails.version unless Rails.version == '2.3.12'
+class ActionController::Base
+  def force_utf8_params
+    traverse = lambda do |object, block|
+      if object.kind_of?(Hash)
+        object.each_value { |o| traverse.call(o, block) }
+      elsif object.kind_of?(Array)
+        object.each { |o| traverse.call(o, block) }
+      else
+        block.call(object)
+      end
+      object
+    end
+    force_encoding = lambda do |o|
+      o.force_encoding(Encoding::UTF_8) if o.respond_to?(:force_encoding)
+    end
+    traverse.call(params, force_encoding)
+  end
+  before_filter :force_utf8_params
+end
