27502d5a1ac134831c6449f9c2812899ee9a6db9
Toshi MARUYAMA <marutosijp2@gmail.com>
2011-08-22 12:37:11 +0900

    Rails3.1: route: replace ActionController::Routing::Routes.draw
    --HG--
    extra : rebase_source : 27c55253d218a13e96beef0b904685d7ca07b954

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@
 # See doc/COPYRIGHT.rdoc for more details.
 #++
 
-ActionController::Routing::Routes.draw do |map|
+Redmine::Application.routes.draw do |map|
   # Add your own custom routes here.
   # The priority is based upon order of creation: first created -> highest priority.
 
