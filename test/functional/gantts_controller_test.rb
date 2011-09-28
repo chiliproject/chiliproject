58f7af17c221f972064d41e8f8d20989dd9d3a97
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-28 10:11:42 +0000

    Rails3: test: template must be specified as controller/action not with their extension any more
    Contributed by Sylvain Utard.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7543 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/gantts_controller_test.rb
+++ test/functional/gantts_controller_test.rb
@@ -62,7 +62,7 @@ class GanttsControllerTest < ActionController::TestCase
 
       get :show, :project_id => 1
       assert_response :success
-      assert_template 'show.html.erb'
+      assert_template 'gantts/show'
       assert_not_nil assigns(:gantt)
       # Issue with start and due dates
       i = Issue.find(1)
@@ -78,7 +78,7 @@ class GanttsControllerTest < ActionController::TestCase
 
       get :show, :project_id => 1
       assert_response :success
-      assert_template 'show.html.erb'
+      assert_template 'gantts/show'
       assert_not_nil assigns(:gantt)
     end
 
@@ -88,14 +88,14 @@ class GanttsControllerTest < ActionController::TestCase
 
       get :show, :project_id => 1
       assert_response :success
-      assert_template 'show.html.erb'
+      assert_template 'gantts/show'
       assert_not_nil assigns(:gantt)
     end
 
     should "work cross project" do
       get :show
       assert_response :success
-      assert_template 'show.html.erb'
+      assert_template 'gantts/show'
       assert_not_nil assigns(:gantt)
       assert_not_nil assigns(:gantt).query
       assert_nil assigns(:gantt).project
@@ -104,7 +104,7 @@ class GanttsControllerTest < ActionController::TestCase
     should "not disclose private projects" do
       get :show
       assert_response :success
-      assert_template 'show.html.erb'
+      assert_template 'gantts/show'
 
       assert_tag 'a', :content => /eCookbook/
       # Root private project
