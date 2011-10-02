0a69d39f346c666e836cf41ae37a71cf2c25f285
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-10-02 09:14:01 +0900

    Rails3: mailer: config/initializers/rails3.rb

--- config/initializers/rails3.rb
+++ config/initializers/rails3.rb
@@ -6,7 +6,7 @@ end
 
 class ActiveRecord::Base
   alias :callback :run_callbacks
-  
+
   def self.merge_conditions(*conditions)
     segments = []
 
@@ -18,5 +18,13 @@ class ActiveRecord::Base
     end
 
     "(#{segments.join(') AND (')})" unless segments.empty?
+  end
+end
+
+# https://gist.github.com/akaspick/rails/commit/60d358b23348a14447d176fa51624ad5434eb575
+class HTML::Document
+  alias :old_initialize :initialize
+  def initialize(doc, *args)
+    old_initialize(doc.to_s, *args)
   end
 end
