f1d740d2501dc541c56204a8f622c00f0889813a
jwollert <johannes.wollert@gmail.com>
2011-10-07 10:51:44 +0200

    Revert "display setting tabs in project menu"
    This reverts commit 323f78287f81a2c1554ce9cf67567ecd01b7c438.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -19,20 +19,17 @@ module ProjectsHelper
   end
 
   def project_settings_tabs
-    available_project_settings_tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
-  end
-
-  def available_project_settings_tabs
-    [{:name => 'info', :action => :edit_project, :partial => 'projects/edit', :label => :label_information_plural},
-      {:name => 'modules', :action => :select_project_modules, :partial => 'projects/settings/modules', :label => :label_module_plural},
-      {:name => 'members', :action => :manage_members, :partial => 'projects/settings/members', :label => :label_member_plural},
-      {:name => 'versions', :action => :manage_versions, :partial => 'projects/settings/versions', :label => :label_version_plural},
-      {:name => 'categories', :action => :manage_categories, :partial => 'projects/settings/issue_categories', :label => :label_issue_category_plural},
-      {:name => 'wiki', :action => :manage_wiki, :partial => 'projects/settings/wiki', :label => :label_wiki},
-      {:name => 'repository', :action => :manage_repository, :partial => 'projects/settings/repository', :label => :label_repository},
-      {:name => 'boards', :action => :manage_boards, :partial => 'projects/settings/boards', :label => :label_board_plural},
-      {:name => 'activities', :action => :manage_project_activities, :partial => 'projects/settings/activities', :label => :enumeration_activities}
-    ]
+    tabs = [{:name => 'info', :action => :edit_project, :partial => 'projects/edit', :label => :label_information_plural},
+            {:name => 'modules', :action => :select_project_modules, :partial => 'projects/settings/modules', :label => :label_module_plural},
+            {:name => 'members', :action => :manage_members, :partial => 'projects/settings/members', :label => :label_member_plural},
+            {:name => 'versions', :action => :manage_versions, :partial => 'projects/settings/versions', :label => :label_version_plural},
+            {:name => 'categories', :action => :manage_categories, :partial => 'projects/settings/issue_categories', :label => :label_issue_category_plural},
+            {:name => 'wiki', :action => :manage_wiki, :partial => 'projects/settings/wiki', :label => :label_wiki},
+            {:name => 'repository', :action => :manage_repository, :partial => 'projects/settings/repository', :label => :label_repository},
+            {:name => 'boards', :action => :manage_boards, :partial => 'projects/settings/boards', :label => :label_board_plural},
+            {:name => 'activities', :action => :manage_project_activities, :partial => 'projects/settings/activities', :label => :enumeration_activities}
+            ]
+    tabs.select {|tab| User.current.allowed_to?(tab[:action], @project)}
   end
 
   def parent_project_select_tag(project)
