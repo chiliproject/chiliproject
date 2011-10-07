b4e688813b0842a24084acb8dc29435793d59177
Romano Licker <r.licker@finn.de>
2011-10-07 17:09:45 +0200

    fixes admin-menu sets main admin page to 'projects' new design on ticket view filter / options / attachments fieldset redesign

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -165,7 +165,7 @@ Redmine::MenuManager.map :top_menu do |menu|
   menu.push :home, :home_path
   menu.push :my_page, { :controller => 'my', :action => 'page' }, :if => Proc.new { User.current.logged? }
   menu.push :projects, { :controller => 'projects', :action => 'index' }, :caption => :label_project_plural
-  menu.push :administration, { :controller => 'admin', :action => 'index' }, :if => Proc.new { User.current.admin? }, :last => true
+  menu.push :administration, { :controller => 'admin', :action => 'projects' }, :if => Proc.new { User.current.admin? }, :last => true
   menu.push :help, Redmine::Info.help_url, :last => true, :caption => "?"
 end
 
