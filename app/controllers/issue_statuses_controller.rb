2830b0f2a8304a74e0c440d47b1501d3c7f64ef2
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-11-22 22:04:07 +0000

    Resourcified issue statuses.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7890 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issue_statuses_controller.rb
+++ app/controllers/issue_statuses_controller.rb
@@ -17,9 +17,6 @@ class IssueStatusesController < ApplicationController
 
   before_filter :require_admin
 
-  verify :method => :post, :only => [ :destroy, :create, :update, :move, :update_issue_done_ratio ],
-         :redirect_to => { :action => :index }
-
   def index
     @issue_status_pages, @issue_statuses = paginate :issue_statuses, :per_page => 25, :order => "position"
     render :action => "index", :layout => false if request.xhr?
@@ -31,7 +28,7 @@ class IssueStatusesController < ApplicationController
 
   def create
     @issue_status = IssueStatus.new(params[:issue_status])
-    if @issue_status.save
+    if request.post? && @issue_status.save
       flash[:notice] = l(:notice_successful_create)
       redirect_to :action => 'index'
     else
@@ -45,7 +42,7 @@ class IssueStatusesController < ApplicationController
 
   def update
     @issue_status = IssueStatus.find(params[:id])
-    if @issue_status.update_attributes(params[:issue_status])
+    if request.put? && @issue_status.update_attributes(params[:issue_status])
       flash[:notice] = l(:notice_successful_update)
       redirect_to :action => 'index'
     else
@@ -53,6 +50,7 @@ class IssueStatusesController < ApplicationController
     end
   end
 
+  verify :method => :delete, :only => :destroy, :redirect_to => { :action => :index }
   def destroy
     IssueStatus.find(params[:id]).destroy
     redirect_to :action => 'index'
@@ -62,7 +60,7 @@ class IssueStatusesController < ApplicationController
   end
 
   def update_issue_done_ratio
-    if IssueStatus.update_issue_done_ratios
+    if request.post? && IssueStatus.update_issue_done_ratios
       flash[:notice] = l(:notice_issue_done_ratios_updated)
     else
       flash[:error] =  l(:error_issue_done_ratios_not_updated)
