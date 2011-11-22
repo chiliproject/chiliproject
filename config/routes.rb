0a4f5c96092790266d03dbf51bef87ec02603f00
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-11-22 21:32:45 +0000

    Resourcified trackers.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7888 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/routes.rb
+++ config/routes.rb
@@ -230,6 +230,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'attachments/download/:id/:filename', :controller => 'attachments', :action => 'download', :id => /\d+/, :filename => /.*/
 
   map.resources :groups
+  map.resources :trackers, :except => :show
 
   #left old routes at the bottom for backwards compat
   map.connect 'projects/:project_id/issues/:action', :controller => 'issues'
