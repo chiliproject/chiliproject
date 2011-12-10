a5db63e2a1097381fae984f721745db54fd70dd9
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-12-10 09:38:13 +0000

    remove no longer working old wiki route
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8154 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/routes.rb
+++ config/routes.rb
@@ -184,7 +184,6 @@ ActionController::Routing::Routes.draw do |map|
 
   #left old routes at the bottom for backwards compat
   map.connect 'boards/:board_id/topics/:action/:id', :controller => 'messages'
-  map.connect 'wiki/:id/:page/:action', :page => nil, :controller => 'wiki'
   map.connect 'issues/:issue_id/relations/:action/:id', :controller => 'issue_relations'
   map.connect 'projects/:project_id/news/:action', :controller => 'news'
 
