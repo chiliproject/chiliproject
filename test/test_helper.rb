10054f112eac27e86b8b39a5807d5488aea42200
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-12-18 09:40:19 +0000

    Get rid of HelperTestcase.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8278 e93f8b46-1217-0410-a6f0-8f06a7374b81
    
    --HG--
    extra : source : 3eb2f1276ca18f771a38f78ad325bfba6d77fef8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,7 +19,6 @@ Coveralls.wear!('rails')
 
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-require File.expand_path(File.dirname(__FILE__) + '/helper_testcase')
 require Rails.root.join('test', 'mocks', 'open_id_authentication_mock.rb')
 
 require File.expand_path(File.dirname(__FILE__) + '/object_helpers')
