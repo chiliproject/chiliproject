2f4d77b45b6abdac8c4d67f62e6742f6a90ea2ea
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-24 10:57:45 +0000

    test: route: move users tests to new file
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8349 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -57,30 +57,6 @@ class RoutingTest < ActionController::IntegrationTest
     should_route :delete, "/projects/redmine/queries/1", :controller => 'queries', :action => 'destroy', :id => '1', :project_id => 'redmine'
   end
 
-  context "users" do
-    should_route :get, "/users", :controller => 'users', :action => 'index'
-    should_route :get, "/users.xml", :controller => 'users', :action => 'index', :format => 'xml'
-    should_route :get, "/users/44", :controller => 'users', :action => 'show', :id => '44'
-    should_route :get, "/users/44.xml", :controller => 'users', :action => 'show', :id => '44', :format => 'xml'
-    should_route :get, "/users/current", :controller => 'users', :action => 'show', :id => 'current'
-    should_route :get, "/users/current.xml", :controller => 'users', :action => 'show', :id => 'current', :format => 'xml'
-    should_route :get, "/users/new", :controller => 'users', :action => 'new'
-    should_route :get, "/users/444/edit", :controller => 'users', :action => 'edit', :id => '444'
-
-    should_route :post, "/users", :controller => 'users', :action => 'create'
-    should_route :post, "/users.xml", :controller => 'users', :action => 'create', :format => 'xml'
-
-    should_route :put, "/users/444", :controller => 'users', :action => 'update', :id => '444'
-    should_route :put, "/users/444.xml", :controller => 'users', :action => 'update', :id => '444', :format => 'xml'
-
-    should_route :delete, "/users/44", :controller => 'users', :action => 'destroy', :id => '44'
-    should_route :delete, "/users/44.xml", :controller => 'users', :action => 'destroy', :id => '44', :format => 'xml'
-
-    should_route :post, "/users/123/memberships", :controller => 'users', :action => 'edit_membership', :id => '123'
-    should_route :put, "/users/123/memberships/55", :controller => 'users', :action => 'edit_membership', :id => '123', :membership_id => '55'
-    should_route :delete, "/users/123/memberships/55", :controller => 'users', :action => 'destroy_membership', :id => '123', :membership_id => '55'
-  end
-
   context "versions" do
     # /projects/foo/versions is /projects/foo/roadmap
     should_route :get, "/projects/foo/versions.xml", :controller => 'versions', :action => 'index', :project_id => 'foo', :format => 'xml'
