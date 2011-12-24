2fd5fc4ebd27c6881f6fa4342dc14044f82d75b8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-24 10:13:27 +0000

    test: route: move auto_completes tests to new file
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8347 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -36,9 +36,6 @@ class RoutingTest < ActionController::IntegrationTest
 
     should_route :get, "/issues/gantt", :controller => 'gantts', :action => 'show'
     should_route :get, "/projects/project-name/issues/gantt", :controller => 'gantts', :action => 'show', :project_id => 'project-name'
-
-    should_route :get, "/issues/auto_complete", :controller => 'auto_completes', :action => 'issues'
-
     should_route :get, "/issues/preview/123", :controller => 'previews', :action => 'issue', :id => '123'
     should_route :post, "/issues/preview/123", :controller => 'previews', :action => 'issue', :id => '123'
     should_route :get, "/issues/context_menu", :controller => 'context_menus', :action => 'issues'
@@ -309,9 +306,5 @@ class RoutingTest < ActionController::IntegrationTest
 
     should_route :post, "/projects/ladida/wiki", :controller => 'wikis', :action => 'edit', :id => 'ladida'
     should_route :post, "/projects/ladida/wiki/destroy", :controller => 'wikis', :action => 'destroy', :id => 'ladida'
-  end
-
-  context "auto_completes" do
-    should_route :get, "/users/auto_complete", :controller => 'auto_completes', :action => 'users'
   end
 end
