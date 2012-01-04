d2cb4572ba62bb6b56d3bf414ccb6ac72ca50e24
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-01-04 08:51:00 +0000

    route: simplify repositories route
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8497 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/routes.rb
+++ config/routes.rb
@@ -193,7 +193,6 @@ ActionController::Routing::Routes.draw do |map|
   map.with_options :controller => 'repositories' do |repositories|
     repositories.with_options :conditions => {:method => :get} do |repository_views|
       repository_views.connect 'projects/:id/repository', :action => 'show'
-      repository_views.connect 'projects/:id/repository/edit', :action => 'edit'
       repository_views.connect 'projects/:id/repository/statistics', :action => 'stats'
       repository_views.connect 'projects/:id/repository/revisions', :action => 'revisions'
       repository_views.connect 'projects/:id/repository/revisions.:format', :action => 'revisions'
@@ -214,7 +213,8 @@ ActionController::Routing::Routes.draw do |map|
 
     repositories.connect 'projects/:id/repository/revision', :action => 'revision', :conditions => {:method => [:get, :post]}
     repositories.connect 'projects/:id/repository/committers', :action => 'committers', :conditions => {:method => [:get, :post]}
-    repositories.connect 'projects/:id/repository/edit', :action => 'edit', :conditions => {:method => :post}
+    repositories.connect 'projects/:id/repository/edit', :action => 'edit',
+                          :conditions => {:method => [:get, :post]}
     repositories.connect 'projects/:id/repository/destroy', :action => 'destroy', :conditions => {:method => :post}
   end
 
