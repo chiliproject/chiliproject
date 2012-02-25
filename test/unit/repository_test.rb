ed27ef115b5883805fc022ef5336c3515f4f5b58
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-02-25 09:58:23 +0000

    Do not do assertions on mail class.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8991 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -100,7 +100,7 @@ class RepositoryTest < ActiveSupport::TestCase
     # 2 email notifications to 5 users
     assert_equal 5, ActionMailer::Base.deliveries.size
     mail = ActionMailer::Base.deliveries.first
-    assert_kind_of TMail::Mail, mail
+    assert_not_nil mail
     assert mail.subject.starts_with?("[#{fixed_issue.project.name} - #{fixed_issue.tracker.name} ##{fixed_issue.id}]")
     assert mail.body.include?("Status changed from #{old_status} to #{fixed_issue.status}")
 
