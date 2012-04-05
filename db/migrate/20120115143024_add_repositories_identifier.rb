1bdfb592dd537d92279dadad46c0c6c07afdada1
root <root@melon.psyop.tv>
2012-04-05 00:07:48 -0400

    added missing db migration files

---
+++ db/migrate/20120115143024_add_repositories_identifier.rb
@@ -0,0 +1,18 @@
+class AddRepositoriesIdentifier < ActiveRecord::Migration
+  def self.up
+    add_column :repositories, :identifier, :string
+  end
+
+  def self.down
+    remove_column :repositories, :identifier
+  end
+end
+class AddRepositoriesIdentifier < ActiveRecord::Migration
+  def self.up
+    add_column :repositories, :identifier, :string
+  end
+
+  def self.down
+    remove_column :repositories, :identifier
+  end
+end
