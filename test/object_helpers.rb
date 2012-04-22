045147dd87cae3940b74b42e869562c017d92adf
jplang <none@none>
2012-04-22 11:58:05 +0000

    Renamed object_daddy_helper and removed exemplars.
    --HG--
    rename : test/object_daddy_helpers.rb => test/object_helpers.rb

--- test/object_helpers.rb
+++ test/object_helpers.rb
@@ -1,18 +1,4 @@
-#-- encoding: UTF-8
-#-- copyright
-# ChiliProject is a project management system.
-#
-# Copyright (C) 2010-2013 the ChiliProject Team
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# See doc/COPYRIGHT.rdoc for more details.
-#++
-
-module ObjectDaddyHelpers
+module ObjectHelpers
   # TODO: Remove these three once everyone has ported their code to use the
   # new object_daddy version with protected attribute support
   def User.generate_with_protected(attributes={})
