8d56d32774bdcc7e2b75d32ad3a25533f36c21b3
Holger Just <hjust@meine-er.de>
2012-06-09 18:17:46 +0200

    Bump to 2.7.2

--- lib/chili_project/version.rb
+++ lib/chili_project/version.rb
@@ -19,7 +19,7 @@ module ChiliProject
 
     MAJOR = 2
     MINOR = 7
-    PATCH = 1
+    PATCH = 2
     TINY  = PATCH # Redmine compat
 
     # Used by semver to define the special version (if any).
