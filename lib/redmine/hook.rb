b75a4da1c0a1624956c1bcaaa7378e8437e6fbae
Holger Just <hjust@meine-er.de>
2012-08-25 12:17:56 +0200

    Don't use the prototype helper anymore
    --HG--
    extra : source : 484d5e5733f3805b9e8e5ba447e75fd900b78fca

--- lib/redmine/hook.rb
+++ lib/redmine/hook.rb
@@ -85,7 +85,6 @@ module Redmine
       include ActionView::Helpers::FormTagHelper
       include ActionView::Helpers::FormOptionsHelper
       include ActionView::Helpers::JavaScriptHelper
-      include ActionView::Helpers::PrototypeHelper
       include ActionView::Helpers::NumberHelper
       include ActionView::Helpers::UrlHelper
       include ActionView::Helpers::AssetTagHelper
