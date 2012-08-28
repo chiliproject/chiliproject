8e3752aaa23d1254896b0ff22e7af25bd0cd80b9
Holger Just <hjust@meine-er.de>
2012-08-28 17:06:35 +0200

    Update to new syntax for validation definitions
    --HG--
    extra : rebase_source : 220a6efd3556ade0726a6c32d102231fbbd0af9f

--- vendor/plugins/acts_as_customizable/lib/acts_as_customizable.rb
+++ vendor/plugins/acts_as_customizable/lib/acts_as_customizable.rb
@@ -32,7 +32,7 @@ module Redmine
                                    :include => :custom_field,
                                    :order => "#{CustomField.table_name}.position",
                                    :dependent => :delete_all
-          before_validation { |customized| customized.custom_field_values if customized.new_record? }
+          before_validation(:on => :create) { |customized| customized.custom_field_values }
           # Trigger validation only if custom values were changed
           validates_associated :custom_values, :on => :update, :if => Proc.new { |customized| customized.custom_field_values_changed? }
           send :include, Redmine::Acts::Customizable::InstanceMethods
