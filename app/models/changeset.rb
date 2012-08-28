e05a86f6258bad78b958b7d34dd2ea1d0314a38c
Holger Just <hjust@meine-er.de>
2012-08-28 17:03:55 +0200

    Replace named_scope with class methods
    --HG--
    extra : rebase_source : b29ffd610e5733a43fa7b9d5cbf0f17f4d07a670

--- app/models/changeset.rb
+++ app/models/changeset.rb
@@ -37,8 +37,9 @@ class Changeset < ActiveRecord::Base
   validates_uniqueness_of :revision, :scope => :repository_id
   validates_uniqueness_of :scmid, :scope => :repository_id, :allow_nil => true
 
-  scope :visible, lambda {|*args| { :include => {:repository => :project},
-                                          :conditions => Project.allowed_to_condition(args.first || User.current, :view_changesets) } }
+  def self.visible(user=User.current)
+    joins(:repository => :project).where(Project.allowed_to_condition(user, :view_changesets))
+  end
 
   after_create :scan_for_issues
 
