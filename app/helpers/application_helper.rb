d3d605248ef056cbb9357bbad19d707ea7251f07
Holger Just <hjust@meine-er.de>
2012-08-29 12:09:49 +0200

    Make output of javascript_heads HTML-safe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -985,7 +985,7 @@ module ApplicationHelper
   def javascript_heads
     tags = javascript_include_tag(:defaults)
     unless User.current.pref.warn_on_leaving_unsaved == '0'
-      tags << "\n" + javascript_tag("Event.observe(window, 'load', function(){ new WarnLeavingUnsaved('#{escape_javascript( l(:text_warn_on_leaving_unsaved) )}'); });")
+      tags << javascript_tag("Event.observe(window, 'load', function(){ new WarnLeavingUnsaved('#{escape_javascript( l(:text_warn_on_leaving_unsaved) )}'); });")
     end
     tags << jquery_datepicker_settings
     tags
