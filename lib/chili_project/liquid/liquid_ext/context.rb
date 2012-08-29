b79b94930fe08a6a6271caef25700fa741c8ac63
Holger Just <hjust@meine-er.de>
2012-08-29 11:40:18 +0200

    SecureRandom moved to the top-level scope in Rails3
    --HG--
    extra : rebase_source : 781b3081eaaa17f96129896944bd14a1cde820b1

--- lib/chili_project/liquid/liquid_ext/context.rb
+++ lib/chili_project/liquid/liquid_ext/context.rb
@@ -35,7 +35,7 @@ module ChiliProject
           def html_result(html)
             key = nil
             while key.nil? || html_results.has_key?(key)
-              random = ActiveSupport::SecureRandom.hex(10)
+              random = SecureRandom.hex(10)
               # This string must be passed untouched through Liquid and textile
               # It mustn't be changed in any way by any rendering stage.
               key = "!!html_results.#{random}!!"
