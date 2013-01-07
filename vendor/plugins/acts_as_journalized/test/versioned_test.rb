bfbe1d720ddd3e1398c7d0c801fa68da51008d64
Greg Thornton <xdissent@me.com>
2013-01-07 00:24:39 -0600

    Fix version tests.

--- vendor/plugins/acts_as_journalized/test/versioned_test.rb
+++ vendor/plugins/acts_as_journalized/test/versioned_test.rb
@@ -1,15 +1,15 @@
 #-- encoding: UTF-8
 require File.join(File.dirname(__FILE__), 'test_helper')
 
-class VersionedTest < Test::Unit::TestCase
+class VersionedTest < ActiveSupport::TestCase
   context 'ActiveRecord models' do
     should 'respond to the "journaled?" method' do
       assert ActiveRecord::Base.respond_to?(:journaled?)
-      assert User.respond_to?(:journaled?)
+      assert TestUser.respond_to?(:journaled?)
     end
 
     should 'return true for the "journaled?" method if the model is journaled' do
-      assert_equal true, User.journaled?
+      assert_equal true, TestUser.journaled?
     end
 
     should 'return false for the "journaled?" method if the model is not journaled' do
