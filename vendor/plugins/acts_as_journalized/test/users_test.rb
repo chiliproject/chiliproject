5e789a3c4a7f5176cadbd4edbc8f3e22c1938b41
Greg Thornton <xdissent@me.com>
2013-01-07 00:08:10 -0600

    Fix users test.

--- vendor/plugins/acts_as_journalized/test/users_test.rb
+++ vendor/plugins/acts_as_journalized/test/users_test.rb
@@ -1,26 +1,23 @@
 #-- encoding: UTF-8
-require 'test_helper'
+require File.join(File.dirname(__FILE__), 'test_helper')
 
-class UsersTest < Test::Unit::TestCase
+class UsersTest < ActiveSupport::TestCase
+  fixtures :users
+
   context 'The user responsible for an update' do
     setup do
-      @updated_by = User.create(:name => 'Steve Jobs')
-      @user = User.create(:name => 'Steve Richert')
+      @updated_by = User.find 1
+      @user = TestUser.create(:name => 'Steve Richert')
     end
 
-    should 'default to nil' do
+    should 'default to User.current' do
       @user.update_attributes(:first_name => 'Stephen')
-      assert_nil @user.journals.last.user
+      assert_equal User.current, @user.journals.last.user
     end
 
-    should 'accept and return an ActiveRecord user' do
-      @user.update_attributes(:first_name => 'Stephen', :updated_by => @updated_by)
+    should 'accept and return a User' do
+      @user.update_attributes(:first_name => 'Stephen', :journal_user => @updated_by)
       assert_equal @updated_by, @user.journals.last.user
-    end
-
-    should 'accept and return a string user name' do
-      @user.update_attributes(:first_name => 'Stephen', :updated_by => @updated_by.name)
-      assert_equal @updated_by.name, @user.journals.last.user
     end
   end
 end
