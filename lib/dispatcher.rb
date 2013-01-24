078f525eb726584c3581c23b7017d6f3249a3bd8
Holger Just <hjust@meine-er.de>
2013-01-24 22:44:13 +0100

    Improve error messages
    --HG--
    extra : rebase_source : 70632016b4897335e120d1cb995bf1b4a02570c2

--- lib/dispatcher.rb
+++ lib/dispatcher.rb
@@ -1,7 +1,8 @@
 class Dispatcher
   def self.to_prepare(&block)
     ::ActiveSupport::Deprecation.warn(
-      "Dispatcher.to_prepare is deprected. Use Rails.configuration.to_prepare instead."
+      "Dispatcher.to_prepare is deprected. Use Rails.configuration.to_prepare instead.",
+      caller
     )
     Rails.configuration.to_prepare(&block)
   end
