deb0d3ab42d16e493a55f17e95a5e8ddcb0fc418
Holger Just <hjust@meine-er.de>
2013-01-24 22:38:13 +0100

    HTTP method verification has to be performed in the routes
    --HG--
    extra : source : 6e31e50cd77e51c6655c64962003580a0db78e81

--- app/controllers/mail_handler_controller.rb
+++ app/controllers/mail_handler_controller.rb
@@ -15,10 +15,6 @@
 class MailHandlerController < ActionController::Base
   before_filter :check_credential
 
-  verify :method => :post,
-         :only => :index,
-         :render => { :nothing => true, :status => 405 }
-
   # Submits an incoming email to MailHandler
   def index
     options = params.dup
