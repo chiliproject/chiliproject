19abf4d1edb2b816adbc5eaf0d1a3a193589922f
Felix Schäfer <felix@fachschaften.org>
2013-06-07 22:04:24 +0200

    Use Rails 3 form_for method signature
    http://api.rubyonrails.org/classes/ActionView/Helpers/FormHelper.html\#method-i-form_for
    
    --HG--
    extra : source : c95cafe696d40b75cf97b76fffd6a40d13e5ed9e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -844,10 +844,10 @@ module ApplicationHelper
     content_tag("label", label_text)
   end
 
-  def labelled_tabular_form_for(name, object, options, &proc)
+  def labelled_tabular_form_for(record, options = {} , &proc)
     options[:html] ||= {}
     options[:html][:class] = 'tabular' unless options[:html].has_key?(:class)
-    form_for(name, object, options.merge({ :builder => TabularFormBuilder, :lang => current_language}), &proc)
+    form_for(record, options.merge({ :builder => TabularFormBuilder, :lang => current_language}), &proc)
   end
 
   def back_url_hidden_field_tag
