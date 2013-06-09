d76b78f267822282d6df1a898aa8bd958f4f3911
Felix Schäfer <felix@fachschaften.org>
2013-06-09 13:47:55 +0200

    Add note about probably unneeded option
    --HG--
    extra : rebase_source : a7cfadfd57350909b71a7a329a3c6ecf48e71cd8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -847,6 +847,7 @@ module ApplicationHelper
   def labelled_tabular_form_for(record, options = {} , &proc)
     options[:html] ||= {}
     options[:html][:class] = 'tabular' unless options[:html].has_key?(:class)
+    # TODO: no mention of lang in actionview, probably not needed anymore
     form_for(record, options.merge({ :builder => TabularFormBuilder, :lang => current_language}), &proc)
   end
 
