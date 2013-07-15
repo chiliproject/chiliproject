c0cbe407f232a08a92b531cf5ce3073f684afe8b
Toshi MARUYAMA <marutosijp2@gmail.com>
2013-07-15 21:48:16 +0900

    replace object_daddy on unit tests
    --HG--
    extra : source : ddf65daf2e9d013063f9a34aee7aae63dde67a29

--- test/unit/issue_drop_test.rb
+++ test/unit/issue_drop_test.rb
@@ -62,7 +62,7 @@ class IssueDropTest < ActiveSupport::TestCase
     @issue = Issue.generate_for_project!(@project)
     User.current = @user = User.generate!
     @role = Role.generate!(:permissions => [:view_issues])
-    Member.generate!(:principal => @user, :project => @project, :roles => [@role])
+    Member.create!(:principal => @user, :project => @project, :roles => [@role])
     @drop = @issue.to_liquid
   end
 
@@ -71,7 +71,6 @@ class IssueDropTest < ActiveSupport::TestCase
       assert @drop.is_a?(IssueDrop), "drop is not a IssueDrop"
     end
   end
-
 
   [
    :tracker,
@@ -96,15 +95,14 @@ class IssueDropTest < ActiveSupport::TestCase
     should "IssueDrop##{attribute} should return the actual #{attribute} attribute" do
       assert @issue.respond_to?(attribute), "Issue does not have an #{attribute} method"
       assert @drop.respond_to?(attribute), "IssueDrop does not have an #{attribute} method"
-
       assert_equal @issue.send(attribute), @drop.send(attribute)
     end
   end
 
   context "custom fields" do
     setup do
-      @field = IssueCustomField.generate!(:name => 'The Name', :field_format => 'string', :is_for_all => true, :trackers => @project.trackers)
-      @field_name_conflict = IssueCustomField.generate!(:name => 'Subject', :field_format => 'string', :is_for_all => true, :trackers => @project.trackers)
+      @field = IssueCustomField.create!(:name => 'The Name', :field_format => 'string', :is_for_all => true, :trackers => @project.trackers)
+      @field_name_conflict = IssueCustomField.create!(:name => 'Subject', :field_format => 'string', :is_for_all => true, :trackers => @project.trackers)
       @issue.custom_fields = [{'id' => @field.id, 'value' => 'Custom field value'},
                               {'id' => @field_name_conflict.id, 'value' => 'Second subject'}]
       assert @issue.save
