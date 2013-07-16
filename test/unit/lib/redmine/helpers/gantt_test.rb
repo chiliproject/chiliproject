9e6078ac1056a24d4f977bae149a883dad91528d
jplang <none@none>
2013-07-16 13:15:25 +0900

    remove object_daddy from tests
    --HG--
    extra : source : 7adabe9800c8cf80808b346446f4a9cda9e7cef4

--- test/unit/lib/redmine/helpers/gantt_test.rb
+++ test/unit/lib/redmine/helpers/gantt_test.rb
@@ -180,7 +180,6 @@ class Redmine::Helpers::GanttTest < ActiveSupport::TestCase
       context "assigned to a shared version of another project" do
         setup do
           p = Project.generate!
-          p.trackers << @tracker
           p.enabled_module_names = [:issue_tracking]
           @shared_version = Version.generate!(:sharing => 'system')
           p.versions << @shared_version
