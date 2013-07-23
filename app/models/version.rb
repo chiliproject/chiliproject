485fb8b44f60f4a7b7cdbd1a0d48f8a299f21115
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-07-23 17:15:49 +0900

    Rails3: named_scope

--- app/models/version.rb
+++ app/models/version.rb
@@ -31,8 +31,8 @@ class Version < ActiveRecord::Base
   validates_inclusion_of :status, :in => VERSION_STATUSES
   validates_inclusion_of :sharing, :in => VERSION_SHARINGS
 
-  named_scope :open, :conditions => {:status => 'open'}
-  named_scope :visible, lambda {|*args| { :include => :project,
+  scope :open, :conditions => {:status => 'open'}
+  scope :visible, lambda {|*args| { :include => :project,
                                           :conditions => Project.allowed_to_condition(args.first || User.current, :view_issues) } }
 
   safe_attributes 'name',
