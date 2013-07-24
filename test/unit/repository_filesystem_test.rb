c32c1367dd20207b48c675dbfbfcf24d4d8f9f0b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-07-24 18:14:58 +0900

    Rails3 compatibility: scm: filesystem: fix error of test_fetch_changesets_from_scratch at unit model test
    On Rails 3.1, following error raises.
    
    <pre>
    test_fetch_changesets_from_scratch(RepositoryGitTest):
    ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection:
     Cannot modify association 'Repository::Git#changes'
     because the source reflection class 'Change' is associated to 'Changeset' via :has_many.
        test/unit/repository_git_test.rb:68:in `test_fetch_changesets_from_scratch'
    </pre>
    
    --HG--
    extra : source : 698fc21d4e36b5205c66590fce821261fb710e06

--- test/unit/repository_filesystem_test.rb
+++ test/unit/repository_filesystem_test.rb
@@ -29,7 +29,7 @@ class RepositoryFilesystemTest < ActiveSupport::TestCase
   if File.directory?(REPOSITORY_PATH)
     def test_fetch_changesets
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
 
       assert_equal 0, @repository.changesets.count
       assert_equal 0, @repository.changes.count
