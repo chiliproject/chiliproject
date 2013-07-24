719403b53cd91772949e2be9eb4e3452b75114f2
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-07-24 13:08:14 +0900

    named_scope: app/models/project.rb
    --HG--
    extra : rebase_source : a202f737e04e83796eb1bd6d838c2601ea376912

--- app/models/project.rb
+++ app/models/project.rb
@@ -78,11 +78,11 @@ class Project < ActiveRecord::Base
 
   before_destroy :delete_all_members
 
-  named_scope :has_module, lambda { |mod| { :conditions => ["#{Project.table_name}.id IN (SELECT em.project_id FROM #{EnabledModule.table_name} em WHERE em.name=?)", mod.to_s] } }
-  named_scope :active, { :conditions => "#{Project.table_name}.status = #{STATUS_ACTIVE}"}
-  named_scope :all_public, { :conditions => { :is_public => true } }
-  named_scope :visible, lambda { { :conditions => Project.visible_by(User.current) } }
-  named_scope :like, lambda {|q|
+  scope :has_module, lambda { |mod| { :conditions => ["#{Project.table_name}.id IN (SELECT em.project_id FROM #{EnabledModule.table_name} em WHERE em.name=?)", mod.to_s] } }
+  scope :active, { :conditions => "#{Project.table_name}.status = #{STATUS_ACTIVE}"}
+  scope :all_public, { :conditions => { :is_public => true } }
+  scope :visible, lambda { { :conditions => Project.visible_by(User.current) } }
+  scope :like, lambda {|q|
     s = "%#{q.to_s.strip.downcase}%"
     {
       :conditions => ["LOWER(name) LIKE ?", s]
