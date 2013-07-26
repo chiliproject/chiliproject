64fc36aa9def7f9d594f4cd0dbd8253141d4fcda
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-07-26 15:29:11 +0900

    Rails3: after_initialize: app/models/custom_value.rb

--- app/models/custom_value.rb
+++ app/models/custom_value.rb
@@ -16,7 +16,8 @@ class CustomValue < ActiveRecord::Base
   belongs_to :custom_field
   belongs_to :customized, :polymorphic => true
 
-  def after_initialize
+  def initialize(attributes=nil, *args)
+    super
     if new_record? && custom_field && (customized_type.blank? || (customized && customized.new_record?))
       self.value ||= custom_field.default_value
     end
