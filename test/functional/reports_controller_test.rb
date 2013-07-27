5f2ebf33e659555537b0200442a0ce73862c2512
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-07-27 14:06:05 +0900

    replace "fixtures :all" in ReportsControllerTest

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -19,7 +19,45 @@ class ReportsController; def rescue_action(e) raise e end; end
 
 
 class ReportsControllerTest < ActionController::TestCase
-  fixtures :all
+  fixtures :attachments,
+           :auth_sources,
+           :boards,
+           :changes,
+           :changesets,
+           :comments,
+           :custom_fields,
+           :custom_fields_projects,
+           :custom_fields_trackers,
+           :custom_values,
+           :documents,
+           :enabled_modules,
+           :enumerations,
+           :groups_users,
+           :issue_categories,
+           :issue_relations,
+           :issue_statuses,
+           :issues,
+           :journals,
+           :member_roles,
+           :members,
+           :messages,
+           :news,
+           :projects,
+           :projects_trackers,
+           :queries,
+           :repositories,
+           :roles,
+           :time_entries,
+           :tokens,
+           :trackers,
+           :user_preferences,
+           :users,
+           :versions,
+           :watchers,
+           :wiki_contents,
+           :wiki_pages,
+           :wikis,
+           :workflows
 
   def setup
     @controller = ReportsController.new
