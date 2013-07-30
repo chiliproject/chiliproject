05152f8c9e749b19481701c11ec9e5d882730a2d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-07-31 00:49:27 +0900

    Rails3: before_save: app/models/repository.rb

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -22,6 +22,7 @@ class Repository < ActiveRecord::Base
   # Raw SQL to delete changesets and changes in the database
   # has_many :changesets, :dependent => :destroy is too slow for big repositories
   before_destroy :clear_changesets
+  before_save :strip_url_and_root_url
 
   validates_length_of :password, :maximum => 255, :allow_nil => true
   # Checks if the SCM is enabled when creating a repository
@@ -279,7 +280,7 @@ class Repository < ActiveRecord::Base
 
   private
 
-  def before_save
+  def strip_url_and_root_url
     # Strips url and root_url
     url.strip!
     root_url.strip!
