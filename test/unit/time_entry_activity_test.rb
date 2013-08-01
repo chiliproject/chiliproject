278720cf0ed51e84e2ecdb98aa0794e3d1efe417
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-08-01 14:36:24 +0900

    Rails3: test: replace deprecated errors.on at test/unit/time_entry_activity_test.rb

--- test/unit/time_entry_activity_test.rb
+++ test/unit/time_entry_activity_test.rb
@@ -68,7 +68,7 @@ class TimeEntryActivityTest < ActiveSupport::TestCase
     # Blanking custom field, save should fail
     e.custom_field_values = {field.id => ""}
     assert !e.save
-    assert e.errors.on(:custom_values)
+    assert e.errors[:custom_values].present?
 
     # Update custom field to valid value, save should succeed
     e.custom_field_values = {field.id => "0"}
