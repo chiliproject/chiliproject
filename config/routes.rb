d32084cb370a7470691ee3fd9f28cbf2bd513fd2
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-08-11 16:35:28 +0900

    route: remove queries resources

--- config/routes.rb
+++ config/routes.rb
@@ -131,7 +131,6 @@ ActionController::Routing::Routes.draw do |map|
     project.resources :news, :shallow => true
     project.resources :time_entries, :controller => 'timelog', :path_prefix => 'projects/:project_id'
     project.resources :boards
-    project.resources :queries, :only => [:new, :create]
     project.resources :issue_categories, :shallow => true
     project.resources :documents, :shallow => true, :member => {:add_attachment => :post}
 
