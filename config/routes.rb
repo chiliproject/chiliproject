18c7290fceb8928c01083f66725ce5e1c5d19d22
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-08-25 01:26:33 +0900

    auto_complete_projects

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,8 @@ ActionController::Routing::Routes.draw do |map|
   # Misc issue routes. TODO: move into resources
   map.auto_complete_issues '/issues/auto_complete', :controller => 'auto_completes', :action => 'issues', :conditions => { :method => :get }
   map.auto_complete_users '/users/auto_complete', :controller => 'auto_completes', :action => 'users', :conditions => { :method => :get }
+  map.auto_complete_projects '/projects/auto_complete', :controller => 'auto_completes', :action => 'projects', :conditions => { :method => :post }
+
   map.preview_issue '/issues/preview/:id', :controller => 'previews', :action => 'issue' # TODO: would look nicer as /issues/:id/preview
   map.issues_context_menu '/issues/context_menu', :controller => 'context_menus', :action => 'issues'
   map.issue_changes '/issues/changes', :controller => 'journals', :action => 'index'
