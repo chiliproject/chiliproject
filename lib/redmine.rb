83b23cc748677120c9bc107ff5878b6d8fbf6838
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-08-25 17:31:49 +0900

    fix board menu

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -342,14 +342,18 @@ Redmine::MenuManager.map :project_menu do |menu|
               :caption => :label_board_plural,
               :if => Proc.new { |p| p.boards.any? },
               :children => Proc.new {|project|
+                children = []
                 project.boards.collect do |board|
-                  Redmine::MenuManager::MenuItem.new(
+                  if !board.new_record?
+                    children << Redmine::MenuManager::MenuItem.new(
                                                      "board-#{board.id}".to_sym,
                                                      { :controller => 'boards', :action => 'show', :project_id => project, :id => board },
                                                      {
                                                        :caption => board.name # is h() in menu_helper.rb
                                                      })
+                   end
                 end
+                children
               }
             })
   menu.push(:new_board, { :controller => 'boards', :action => 'new' }, {
