a38b3022d99ea3f777addc29da86dac003e320ab
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-08-27 10:59:00 +0900

    fix

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -39,9 +39,9 @@ class RoutingTest < ActionController::IntegrationTest
     should_route :get, "/enumerations", :controller => 'enumerations', :action => 'index'
     should_route :get, "/enumerations/new", :controller => 'enumerations', :action => 'new'
     should_route :post, "/enumerations", :controller => 'enumerations', :action => 'create'
-    should_route :get, "/enumerations/2/edit", :controller => 'enumerations', :action => 'edit', :id => 2
-    should_route :put, "/enumerations/2", :controller => 'enumerations', :action => 'update', :id => 2
-    should_route :delete, "/enumerations/2", :controller => 'enumerations', :action => 'destroy', :id => 2
+    should_route :get, "/enumerations/2/edit", :controller => 'enumerations', :action => 'edit', :id => '2'
+    should_route :put, "/enumerations/2", :controller => 'enumerations', :action => 'update', :id => '2'
+    should_route :delete, "/enumerations/2", :controller => 'enumerations', :action => 'destroy', :id => '2'
   end
   
   context "issues" do
